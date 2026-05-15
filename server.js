4af59722578ec7abda1d1127637a51c9a6f43551
Anrogon <153125556+Anrogon@users.noreply.github.com>
2026-05-15 18:12:55 -0300

    room.matchPot

--- server.js
+++ server.js
@@ -2211,7 +2211,7 @@ function createPlayerForSeat(room, seat, clientId, client, avatarUrl) {
   client.chipsBalance = client.chips;
 
   room.matchPot = Number(room.matchPot) || 0;
-  room.matchPot += buyIn;
+  room.matchPot -= buyIn;
 
   room.playersBySeat[seat - 1] = {
     clientId,
